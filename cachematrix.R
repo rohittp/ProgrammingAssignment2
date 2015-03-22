c489f432899a7371bc3ab2c7300db1b900a68a32
rohittp <rohittp@gmail.com>
2015-03-22 01:58:35 -0400

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix & cacheSolve calculate the inverse of a matrix. To avoid potentially time-consuming subsequesnt computations, they cache the inverse of the matrix. 
+## If the inverse is found in the cache and the matrix has not changed, the inverse from cache is retrived rather than recalculating the inverse.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix caches the inverse of a matrix and returns a special "matrix" object with a list of functions to set & get a matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-matrix_inverse<-NULL
-set_matrix <- function(y) {
-  x <<- y
-  matrix_inverse <<- NULL
+    
+    matrix_inverse<-NULL                                               ## Initializing the local variable matrix_inverse to null                     
+    set_matrix <- function(y) {                                        ## Function resets the matrix to a new value and inverse of the matrix to null in the cache.
+        x <<- y                                                        ## Set the matrix to a new value 'y' and store in cache which is the parent environment of the functions.
+        matrix_inverse <<- NULL                                        ## set the inverse of matrix to null in the parent environment.
+    }
+    get_matrix <- function()x                                          ## Function to retrive the matrix
+    setinv_matrix <- function(inverse) matrix_inverse <<- inverse      ## Function to set the inverse of matrix in the cache
+    getinverse_matrix <- function() matrix_inverse                     ## Function to get the inverse of matrix from cache.
+    list(set_matrix = set_matrix, get_matrix = get_matrix,             ## A list of functions is returned.
+         setinv_matrix = setinv_matrix,
+         getinverse_matrix = getinverse_matrix)
 }
-get_matrix <- function() x
-setinv_matrix <- function(inverse) matrix_inverse <<- inverse
-getinverse_matrix <- function() matrix_inverse
-list(set_matrix = set_matrix, get_matrix = get_matrix,
-     setinv_matrix = setinv_matrix,
-     getinverse_matrix = getinverse_matrix)
+
+
+## The function cacheSolve calculates the inverse of the special "matrix" object returned by makeCacheMatrix. If inverse of matrix is already existing in cache
+## and matrix has not changed, it will retrieve the inverse from the cache. 
+
+cacheSolve <- function(x, ...) {                                      ## Function takes  the input variable 'x' as the special "matrix" object returned by makeCacheMatrix
+    matrix_inverse <- x$getinverse_matrix()                           ## Get matrix inverse from the cache
+    if(!is.null(matrix_inverse)) {                                    ## Check if matrix inverse exists in cache
+        message("getting cached data")                                ## Print message indicating returned inverse matrix is from cache
+        return(matrix_inverse)                                        ## Return matrix inverse from cache
+    }                                       
+    matrix<-x$get_matrix()                                            ## Get the matrix value from the object returned by makeCacheMatrix
+    matrix_inverse<-solve(matrix)                                     ## Compute the matrix inverse, this is executed when no matrix is found in cache.
+    x$setinv_matrix(matrix_inverse)                                   ## Set the matrix inverse in the cache
+    matrix_inverse                                                    ## Return matrix inverse
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  matrix_inverse <- x$getinverse_matrix()
-  if(!is.null(matrix_inverse)) {
-    message("getting cached data")
-    return(matrix_inverse)
-  }
-    matrix<-x$get_matrix()
-    matrix_inverse<-solve(matrix)
-    x$setinv_matrix(matrix_inverse)
-   matrix_inverse
-  }
 
